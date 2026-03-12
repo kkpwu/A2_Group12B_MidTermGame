2e079174e6ef1c34f9df98d27c7be7e6ee5d5ec4
kkpwu <145052681+kkpwu@users.noreply.github.com>
2026-03-12 11:38:14 -0400

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -1,7 +1,7 @@
 let rules =
-  "• Click the colored squares into the grid to match the pattern on the target.\n" +
+  "• Click the colored squares in the main grid to match the pattern on the target grid.\n" +
   "• You have 60 seconds to finish.\n" +
-  "• Pop-ups will distract yo - click them to dismiss!\n" +
+  "• Pop-ups will distract you - click them to dismiss!\n" +
   "• Press SPACE to take a deep breath (Pause).\n" +
   "• Press R to restart the game.\n" +
   "• Match the grid perfectly to win.";
